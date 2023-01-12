1548fb4ebe1d76ca99ed075bc78502a35300a659
BigDogStatusMoves <111360939+BigDogStatusMoves@users.noreply.github.com>
2023-01-12 07:58:10 -0600

    j

--- app/src/main/java/com/example/skuta/orientationassignment/MainActivity.kt
+++ app/src/main/java/com/example/skuta/orientationassignment/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.skuta.orientationassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
+    lateinit var editText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        editText = findViewById(R.id.edit_text)
+        button.setOnClickListener {
+            button.text = editText.text
+        }
     }
 }
